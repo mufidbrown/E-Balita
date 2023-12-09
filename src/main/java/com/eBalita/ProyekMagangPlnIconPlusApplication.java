8e910e4d9bda93095be399dd039eb07da8a65740
mufidbrown <ilhammufid67@gmail.com>
2023-12-09 23:57:30 +0700

    First Commit to Github

---
+++ src/main/java/com/eBalita/ProyekMagangPlnIconPlusApplication.java
@@ -0,0 +1,13 @@
+package com.eBalita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyekMagangPlnIconPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyekMagangPlnIconPlusApplication.class, args);
+	}
+
+}
