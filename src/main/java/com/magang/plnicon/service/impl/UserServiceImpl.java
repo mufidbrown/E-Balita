1302ba88b234f71f77aec69f07b2300167399c74
mufidbrown <ilhammufid67@gmail.com>
2023-12-10 14:29:32 +0700

    Create Service UserServiceImpl class

---
+++ src/main/java/com/magang/plnicon/service/impl/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.magang.plnicon.service.impl;
+
+import com.magang.plnicon.entity.User;
+import com.magang.plnicon.repository.UserRepository;
+import com.magang.plnicon.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+/**
+ * Implementation of the {@link UserService} interface for managing user-related operations.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    /**
+     * Finds a user by their username.
+     *
+     * @param username The username to search for.
+     * @return An {@link Optional} containing the {@link User} if found, or an empty {@link Optional} if not found.
+     */
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    /**
+     * Finds a user by their unique identifier.
+     *
+     * @param userId The unique identifier of the user.
+     * @return An {@link Optional} containing the {@link User} if found, or an empty {@link Optional} if not found.
+     */
+    @Override
+    public Optional<User> findById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    /**
+     * Finds a user by their email address.
+     *
+     * @param email The email address to search for.
+     * @return An {@link Optional} containing the {@link User} if found, or an empty {@link Optional} if not found.
+     */
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+}
+
