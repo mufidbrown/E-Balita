0e511764469da151928747052732be77289e195f
mufidbrown <ilhammufid67@gmail.com>
2023-12-11 10:21:39 +0700

    Create AuthServiceImpl

---
+++ src/main/java/com/magang/plnicon/service/impl/AuthServiceImpl.java
@@ -0,0 +1,23 @@
+package com.magang.plnicon.service.impl;
+
+import com.magang.plnicon.repository.UserRepository;
+import com.magang.plnicon.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final RefreshTokenService refreshTokenService;
+
+    private final JwtUtils jwtUtils;
+}
