2abb229890119fbe4b898d605f7fc58c76649394
mufidbrown <ilhammufid67@gmail.com>
2023-12-11 10:37:54 +0700

    Add Exception & update AuthServiceImpl register

---
+++ src/main/java/com/magang/plnicon/exception/orangtua/EmailAlreadyExistsException.java
@@ -0,0 +1,34 @@
+package com.magang.plnicon.exception.orangtua;
+
+
+import com.magang.plnicon.entity.User;
+import com.magang.plnicon.exception.AlreadyException;
+
+import java.io.Serial;
+
+/**
+ * Thrown when the specified {@link User#getEmail()} already exists.
+ */
+public class EmailAlreadyExistsException extends AlreadyException {
+    @Serial
+    private static final long serialVersionUID = -2709807490567608538L;
+
+    private static final String DEFAULT_MESSAGE =
+            "The specified email already exists";
+
+    private static final String MESSAGE_TEMPLATE =
+            "Email already exists: ";
+
+    /**
+     * Constructs an {@code AlreadyException} with the specified email.
+     *
+     * @param email The email.
+     */
+    public EmailAlreadyExistsException(String email) {
+        super(MESSAGE_TEMPLATE.concat(email));
+    }
+
+    public EmailAlreadyExistsException() {
+        super(DEFAULT_MESSAGE);
+    }
+}
--- src/main/java/com/magang/plnicon/service/impl/AuthServiceImpl.java
+++ src/main/java/com/magang/plnicon/service/impl/AuthServiceImpl.java
@@ -1,5 +1,8 @@
 package com.magang.plnicon.service.impl;
 
+import com.magang.plnicon.entity.User;
+import com.magang.plnicon.exception.orangtua.EmailAlreadyExistsException;
+import com.magang.plnicon.payload.request.auth.SignupRequest;
 import com.magang.plnicon.repository.UserRepository;
 import com.magang.plnicon.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +20,31 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
 
-    private final RefreshTokenService refreshTokenService;
+    /**
+     * Registers a new user based on the provided signup request.
+     *
+     * @param request The signup request containing user registration information.
+     * @return A string representing the result of the registration process.
+     */
+    @Override
+    public String register(SignupRequest request) {
 
-    private final JwtUtils jwtUtils;
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException(request.getEmail());
+        }
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        userRepository.save(user);
+
+        return "success";
+    }
+
+
 }
