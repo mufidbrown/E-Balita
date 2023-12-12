fe2862d8ca17d983bd0bacda02d2b17df82c6303
mufidbrown <ilhammufid67@gmail.com>
2023-12-12 08:13:04 +0700

    Add Auth Logout LogoutRequest

---
+++ src/main/java/com/magang/plnicon/payload/request/auth/LogoutRequest.java
@@ -0,0 +1,16 @@
+package com.magang.plnicon.payload.request.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a request object for user logout.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutRequest {
+
+    private String token;
+}
