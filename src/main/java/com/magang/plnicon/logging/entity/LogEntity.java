d98e69e80ce3b13da3241ccc108ad73bf08b6024
mufidbrown <ilhammufid67@gmail.com>
2023-12-15 22:51:09 +0700

    Add Log Logging Entity

---
+++ src/main/java/com/magang/plnicon/logging/entity/LogEntity.java
@@ -0,0 +1,45 @@
+package com.magang.plnicon.logging.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+/**
+ * Entity class representing a {@link LogEntity} entry in the database.
+ */
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "BALITA_DELIVERY_LOGS")
+public class LogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    private String endpoint;
+
+    private String method;
+
+    @Enumerated(EnumType.STRING)
+    private HttpStatus status;
+
+    private String userInfo;
+
+    private String errorType;
+
+    @Column(columnDefinition = "TEXT")
+    private String response;
+
+    private String operation;
+
+    private LocalDateTime time;
+}
