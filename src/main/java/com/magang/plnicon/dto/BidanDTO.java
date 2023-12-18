1b2f0eabe4fb1c4c5087eee0bf9400f68b4e0b94
mufidbrown <ilhammufid67@gmail.com>
2023-12-18 10:02:54 +0700

    Add Bidan, BidanRecordDTO & BidanMapper

---
+++ src/main/java/com/magang/plnicon/dto/BidanDTO.java
@@ -0,0 +1,24 @@
+package com.magang.plnicon.dto;
+
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Data Transfer Object (DTO) representing bidan information.
+ */
+@Getter
+@Builder
+@EqualsAndHashCode
+public class BidanDTO {
+
+    private Long id;
+    private UserDTO user;
+    private LocalDateTime createdAt;
+    private List<BidanRecordDTO> bidanRecords;
+
+}
--- src/main/java/com/magang/plnicon/entity/mapper/bidan/BidanMapper.java
+++ src/main/java/com/magang/plnicon/entity/mapper/bidan/BidanMapper.java
@@ -1,4 +1,13 @@
 package com.magang.plnicon.entity.mapper.bidan;
 
+
+import com.magang.plnicon.dto.BidanDTO;
+import com.magang.plnicon.entity.Bidan;
+import lombok.experimental.UtilityClass;
+
+/**
+ * Utility class for mapping operations related to {@link Bidan} and {@link BidanDTO}.
+ */
+@UtilityClass
 public class BidanMapper {
 }
