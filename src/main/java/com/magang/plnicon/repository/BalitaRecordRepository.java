3fc2181b33d2e3c10e4e8835c9a1a36d06e4117d
mufidbrown <ilhammufid67@gmail.com>
2023-12-18 20:51:13 +0700

    Add BalitaRecordService Interface & Update BalitaRecordRepository

--- src/main/java/com/magang/plnicon/repository/BalitaRecordRepository.java
+++ src/main/java/com/magang/plnicon/repository/BalitaRecordRepository.java
@@ -1,7 +1,27 @@
 package com.magang.plnicon.repository;
 
 import com.magang.plnicon.entity.BalitaRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface BalitaRecordRepository extends JpaRepository<BalitaRecord, Long> {
+
+    Optional<BalitaRecord> findFirstByBalitaBalitaIdOrderByDateCreatedDesc(@Param("balitaId") long balitaId);
+
+    Page<BalitaRecord> findByBalitaBalitaId(Long balitaId, Pageable pageable);
+
+    List<BalitaRecord> findByBalitaBalitaId(Long balitaId);
+
+    int countByDateCreatedAndBalitaBidanBidanId(LocalDate createdAt, long bidanId);
+
+    int countByDateCreatedBetweenAndBalitaBidanBidanId(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("bidanId") Long bidanId);
 }
