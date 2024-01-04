d385f2271db5ffecaba1c809da10bc2cda02e204
mufidbrown <ilhammufid67@gmail.com>
2024-01-04 10:07:27 +0700

    Add Payload Request Pengguna

---
+++ src/main/java/com/magang/plnicon/payload/request/Pengguna.java
@@ -0,0 +1,47 @@
+package com.magang.plnicon.payload.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pengguna {
+
+    @NotBlank
+    private String nama_lengkap;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String roles;
+
+    @NotBlank
+    private String puskesmas;
+
+    @NotBlank
+    private String kelurahan;
+
+    @NotBlank
+    private String kecamatan;
+
+    @NotBlank
+    private String kabupaten;
+
+    @NotBlank
+    private String provinsi;
+
+    @NotBlank
+    private String keterangan;
+
+    @NotBlank
+    private String status;
+}
