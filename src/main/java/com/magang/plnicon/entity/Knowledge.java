44175f25b1bfe350d66d5bffdd476f0f7379580d
mufidbrown <ilhammufid67@gmail.com>
2024-01-04 10:47:43 +0700

    Add Knowledge entity

---
+++ src/main/java/com/magang/plnicon/entity/Knowledge.java
@@ -0,0 +1,21 @@
+package com.magang.plnicon.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+public class Knowledge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String topik;
+    private String keterangan;
+    private String deskripsi;
+}
