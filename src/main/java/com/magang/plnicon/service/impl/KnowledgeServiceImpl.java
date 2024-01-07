4bf2e57624338f571aee0a1fa3e031d3ec5ce2c1
mufidbrown <ilhammufid67@gmail.com>
2024-01-07 23:06:42 +0700

    update KnowledgeServiceImpl

--- src/main/java/com/magang/plnicon/service/impl/KnowledgeServiceImpl.java
+++ src/main/java/com/magang/plnicon/service/impl/KnowledgeServiceImpl.java
@@ -55,8 +55,39 @@ public class KnowledgeServiceImpl implements KnowledgeService {
         }
     }
 
+    @Override
+    public Knowledge updateKnowledge(Integer id, Knowledge newKnowledge) {
+        Optional<Knowledge> existingKnowledgeOptional = knowledgeRepository.findById(id);
+        if (existingKnowledgeOptional.isPresent()) {
+            Knowledge existingKnowledge = existingKnowledgeOptional.get();
+            // Lakukan pembaruan pada atribut yang diperlukan
+            existingKnowledge.setTopik(newKnowledge.getTopik());
+            existingKnowledge.setKeterangan(newKnowledge.getKeterangan());
+            existingKnowledge.setDeskripsi(newKnowledge.getDeskripsi());
+
+            return knowledgeRepository.save(existingKnowledge);
+        } else {
+            throw new EntityNotFoundException("Knowledge dengan ID " + id + " tidak ditemukan");
+        }
+    }
+
 
     @Override
+    public void deleteKnowledge(Integer id) {
+        Optional<Knowledge> existingKnowledgeOptional = knowledgeRepository.findById(id);
+        if (existingKnowledgeOptional.isPresent()) {
+            knowledgeRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Knowledge dengan ID " + id + " tidak ditemukan");
+        }
+    }
+
+}
+
+
+
+
+/*    @Override
     public Knowledge updateKnowledge(Integer id, Knowledge newKnowledge) {
         Optional<Knowledge> existingKnowledgeOptional = knowledgeRepository.findById(id);
         if (existingKnowledgeOptional.isPresent()) {
@@ -69,18 +100,17 @@ public class KnowledgeServiceImpl implements KnowledgeService {
             return knowledgeRepository.save(existingKnowledge);
         }
         return null;
-    }
+    }*/
 
 
-    @Override
-    public void deleteKnowledge(Integer id) {
-        Optional<Knowledge> existingKnowledgeOptional = knowledgeRepository.findById(id);
-        if (existingKnowledgeOptional.isEmpty()) {
-            throw new EntityNotFoundException("Knowledge dengan ID " + id + " tidak ditemukan");
-            // Atau Anda bisa menentukan penanganan kesalahan yang sesuai
-        }
-        knowledgeRepository.deleteById(id);
-    }
 
+//    @Override
+//    public void deleteKnowledge(Integer id) {
+//        Optional<Knowledge> existingKnowledgeOptional = knowledgeRepository.findById(id);
+//        if (existingKnowledgeOptional.isEmpty()) {
+//            throw new EntityNotFoundException("Knowledge dengan ID " + id + " tidak ditemukan");
+//            // Atau Anda bisa menentukan penanganan kesalahan yang sesuai
+//        }
+//        knowledgeRepository.deleteById(id);
+//    }
 
-}
