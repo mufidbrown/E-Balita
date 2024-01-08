6ae1da7e2dbd80ab2c322eccce69c87770658bb0
mufidbrown <ilhammufid67@gmail.com>
2024-01-08 14:27:47 +0700

    Add PayloadRequest Antropometri & Update Antropometri Entity

--- src/main/java/com/magang/plnicon/payload/request/AntropometriRequest.java
+++ src/main/java/com/magang/plnicon/payload/request/AntropometriRequest.java
@@ -1,32 +1,51 @@
-package com.magang.plnicon.entity;
+package com.magang.plnicon.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 
-@Entity
-@Table(name = "Antropometri")
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
-public class Antropometri {
+public class AntropometriRequest {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @NotBlank
     private String nama_balita;
+
+    @NotBlank
     private String tanggal_ambil_sampel;
+
+    @NotBlank
     private String usia_balita;
+
+    @NotBlank
     private String berat_badan;
+
+    @NotBlank
     private String tinggi_badan;
+
+    @NotBlank
     private String lingkar_kepala;
+
+    @NotBlank
     private String lingkar_lengan;
+
+    @NotBlank
     private String lingkar_dada;
+
+    @NotBlank
     private String suhu_badan;
+
+    @NotBlank
     private String hasil;
+
+    @NotBlank
     private String foto;
+
+    @NotBlank
     private String keterangan;
 
 }
-
