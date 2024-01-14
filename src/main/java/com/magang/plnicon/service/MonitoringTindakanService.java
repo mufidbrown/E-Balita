6fe8f64e0a2cb219d55a42437c59f7b1f7759e6b
mufidbrown <ilhammufid67@gmail.com>
2024-01-14 14:54:43 +0700

    update Monitoring Tindakan ServiceIntf & Impl

--- src/main/java/com/magang/plnicon/service/MonitoringTindakanService.java
+++ src/main/java/com/magang/plnicon/service/MonitoringTindakanService.java
@@ -1,4 +1,15 @@
 package com.magang.plnicon.service;
 
+import com.magang.plnicon.entity.MonitoringTindakan;
+
+import java.util.List;
+
 public interface MonitoringTindakanService {
+
+    List<MonitoringTindakan> getAllMonitoringTindakan();
+    MonitoringTindakan getMonitoringTindakanById(Integer id);
+    MonitoringTindakan createMonitoringTindakan(MonitoringTindakan monitoringTindakan);
+    MonitoringTindakan updateMonitoringTindakan(Integer id, MonitoringTindakan monitoringTindakan);
+    void deleteMonitoringTindakan(Integer id);
+
 }
--- src/main/java/com/magang/plnicon/service/impl/MonitoringTindakanServiceImpl.java
+++ src/main/java/com/magang/plnicon/service/impl/MonitoringTindakanServiceImpl.java
@@ -1,9 +1,75 @@
 package com.magang.plnicon.service.impl;
 
+import com.magang.plnicon.entity.MonitoringTindakan;
+import com.magang.plnicon.repository.MonitoringTindakanRepository;
 import com.magang.plnicon.service.MonitoringTindakanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class MonitoringTindakanServiceImpl implements MonitoringTindakanService {
+
+    private final MonitoringTindakanRepository monitoringTindakanRepository;
+
+    @Autowired
+    public MonitoringTindakanServiceImpl(MonitoringTindakanRepository monitoringTindakanRepository) {
+        this.monitoringTindakanRepository = monitoringTindakanRepository;
+    }
+
+    @Override
+    public List<MonitoringTindakan> getAllMonitoringTindakan() {
+        List<MonitoringTindakan> allMonitoringTindakan = monitoringTindakanRepository.findAll();
+        if (!allMonitoringTindakan.isEmpty()) {
+            return allMonitoringTindakan;
+        } else {
+            throw new IllegalArgumentException("Daftar MonitoringTindakan kosong");
+        }
+    }
+
+    @Override
+    public MonitoringTindakan getMonitoringTindakanById(Integer id) {
+        if (id != null) {
+            Optional<MonitoringTindakan> monitoringTindakanOptional = monitoringTindakanRepository.findById(id);
+            if (monitoringTindakanOptional.isPresent()) {
+                return monitoringTindakanOptional.get();
+            } else {
+                throw new IllegalArgumentException("MonitoringTindakan dengan ID " + id + " tidak ditemukan");
+            }
+        } else {
+            throw new IllegalArgumentException("ID MonitoringTindakan tidak valid");
+        }
+    }
+
+    @Override
+    public MonitoringTindakan createMonitoringTindakan(MonitoringTindakan monitoringTindakan) {
+        if (monitoringTindakan.getId() != null) {
+            throw new IllegalArgumentException("Tidak dapat membuat MonitoringTindakan dengan ID yang sudah ditentukan");
+        } else {
+            return monitoringTindakanRepository.save(monitoringTindakan);
+        }
+    }
+
+    @Override
+    public MonitoringTindakan updateMonitoringTindakan(Integer id, MonitoringTindakan monitoringTindakan) {
+        if (monitoringTindakanRepository.existsById(id)) {
+            monitoringTindakan.setId(id); // Pastikan ID yang diset pada objek pengguna sesuai dengan ID yang ingin diperbarui
+            return monitoringTindakanRepository.save(monitoringTindakan);
+        } else {
+            throw new IllegalArgumentException("MonitoringTindakan dengan ID " + id + " tidak ditemukan");
+        }
+    }
+
+    @Override
+    public void deleteMonitoringTindakan(Integer id) {
+        if (monitoringTindakanRepository.existsById(id)) {
+            monitoringTindakanRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("MonitoringTindakan dengan ID " + id + " tidak ditemukan");
+        }
+    }
 }
+
