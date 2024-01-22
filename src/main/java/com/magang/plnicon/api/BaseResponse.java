91791cc807deda7bb42ef2c1e67f02b1a6e3fd7c
mufidbrown <ilhammufid67@gmail.com>
2024-01-22 08:27:36 +0700

    update BaseResponse

--- src/main/java/com/magang/plnicon/api/BaseResponse.java
+++ src/main/java/com/magang/plnicon/api/BaseResponse.java
@@ -1,4 +1,48 @@
 package com.magang.plnicon.api;
 
-public class BaseResponse {
+import io.micrometer.common.util.StringUtils;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponse<T> {
+
+    private boolean success;
+    private String message;
+    @JsonIgnoreProperties({"pageable", "sort"})
+    private T data;
+
+
+    public static <T> BaseResponse<T> ok(T data) {
+        return BaseResponse.<T>builder()
+                .success(true)
+                .message("OK")
+                .data(data)
+                .build();
+    }
+
+    public static <T> BaseResponse<T> ok(String message, T data) {
+        return BaseResponse.<T>builder()
+                .success(true)
+                .message(StringUtils.isNotBlank(message) ? message : "")
+                .data(data)
+                .build();
+    }
+
+
+
+    public static <T> BaseResponse<T> error(String message) {
+        return BaseResponse.<T>builder()
+                .success(false)
+                .message(StringUtils.isNotBlank(message) ? message : "")
+                .data(null)
+                .build();
+    }
+
 }
